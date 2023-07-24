d48c36ccee134833b3f661b6347be4b1afb92f27
Severin Lochschmidt <seviron53@gmail.com>
2023-07-24 23:08:33 +0200

    Add hardware configuration

--- platforms/nuc.nix
+++ platforms/nuc.nix
@@ -2,17 +2,39 @@
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
-    ./hardware-configuration.nix
   ];
 
   config = {
     networking.hostName = "tomnuc";
+
+    networking.useDHCP = lib.mkDefault true;
+    # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+    # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
     # Picked from nixos-hardware
     hardware.cpu.intel.updateMicrocode =
       lib.mkDefault config.hardware.enableRedistributableFirmware;
 
     boot.initrd.kernelModules = [ "i915" ];
+    boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+    fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+
+    swapDevices =
+    [ { device = "/dev/disk/by-label/swap"; }
+    ];
 
     environment.variables = {
       VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
