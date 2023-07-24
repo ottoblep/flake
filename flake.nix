b3bce0e56a7f8d8ba6d5ad7156ac766045461113
Severin Lochschmidt <seviron53@gmail.com>
2023-07-24 23:04:35 +0200

    Fix home-manager release

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
