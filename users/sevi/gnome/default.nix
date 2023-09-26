6647200ae35e22c2e02da119316f085cb8cfefae
Severin Lochschmidt <seviron53@gmail.com>
2023-09-26 21:39:47 +0200

    Add lrz-sync-share desktop item

--- users/sevi/gnome/default.nix
+++ users/sevi/gnome/default.nix
@@ -2,13 +2,24 @@
 {
   imports = [ ./dconf.nix ];
 
-  home.packages = with pkgs.gnomeExtensions; [
-    user-themes
-    paperwm
-    bing-wallpaper-changer
-    just-perfection
-    unite
-    pkgs.dconf2nix
-    pkgs.orchis-theme
-  ];
+  home.packages =
+    let
+      lrzSnSDesktopItem = pkgs.makeDesktopItem
+        {
+          name = "lrz-sync-share";
+          desktopName = "LRZ Sync and Share";
+          exec = "lrz-sync-share";
+        };
+    in
+    with pkgs.gnomeExtensions;
+    [
+      user-themes
+      paperwm
+      bing-wallpaper-changer
+      just-perfection
+      unite
+      pkgs.dconf2nix
+      pkgs.orchis-theme
+      lrzSnSDesktopItem
+    ];
 }
