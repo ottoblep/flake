af7a4ef006f97a95919dc98d9bf073474553a6c3
Severin Lochschmidt <seviron53@gmail.com>
2024-02-20 22:00:56 +0100

    Run formatter

--- platforms/stele.nix
+++ platforms/stele.nix
@@ -33,7 +33,7 @@
       ];
 
       # Load nvidia driver for Xorg and Wayland
-      services.xserver.videoDrivers = ["nvidia"];
+      services.xserver.videoDrivers = [ "nvidia" ];
 
       hardware.nvidia = {
 
@@ -56,7 +56,7 @@
         open = false;
 
         # Enable the Nvidia settings menu,
-	      # accessible via `nvidia-settings`.
+        # accessible via `nvidia-settings`.
         nvidiaSettings = true;
 
         # Optionally, you may need to select the appropriate driver version for your specific GPU.
