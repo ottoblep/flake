ba5faaf5095fe2f9fb6f5cd37b35152ff3fb795e
Severin Lochschmidt <seviron53@gmail.com>
2024-02-20 22:00:56 +0100

    Run formatter

--- users/sevi/home.nix
+++ users/sevi/home.nix
@@ -27,24 +27,24 @@
   };
 
   home.file.".config/btop/btop.conf".source = ./dotfiles/btop.conf;
-  home.file.".config/btop/themes".source = 
-  let
-    src = pkgs.fetchFromGitHub {
-      repo = "btop";
-      owner = "catppuccin";
-      rev = "1.0.0";
-      hash = "sha256-J3UezOQMDdxpflGax0rGBF/XMiKqdqZXuX4KMVGTxFk=";
-    };
-  in 
-  "${src}/themes";
+  home.file.".config/btop/themes".source =
+    let
+      src = pkgs.fetchFromGitHub {
+        repo = "btop";
+        owner = "catppuccin";
+        rev = "1.0.0";
+        hash = "sha256-J3UezOQMDdxpflGax0rGBF/XMiKqdqZXuX4KMVGTxFk=";
+      };
+    in
+    "${src}/themes";
 
   home.shellAliases = {
-   rm = "rm -i";
-   srm = "srm -i";
-   ll = "ls -l";
-   ls = "ls --color=tty";
-   "..." = "cd ../..";
-   btop = "btop -p 0";
+    rm = "rm -i";
+    srm = "srm -i";
+    ll = "ls -l";
+    ls = "ls --color=tty";
+    "..." = "cd ../..";
+    btop = "btop -p 0";
   };
 
   programs.home-manager.enable = true;
