7e61f48dd2607eb78fbfd873873d925777834fa8
Severin Lochschmidt <seviron53@gmail.com>
2024-02-24 16:46:09 +0100

    Add usb module to base trait

--- platforms/stele.nix
+++ platforms/stele.nix
@@ -10,7 +10,7 @@
 
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-      boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+      boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" ];
       boot.initrd.kernelModules = [ ];
       boot.kernelModules = [ "kvm-intel" ];
       boot.extraModulePackages = [ ];
