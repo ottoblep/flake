49a6e39c0ee0d4ff3d91d5d84f1d20a2917d947f
Severin Lochschmidt <seviron53@gmail.com>
2024-03-03 20:43:12 +0100

    Add automatic wallpaper update and color-scheme quantization

--- users/sevi/gnome/default.nix
+++ users/sevi/gnome/default.nix
@@ -51,6 +51,40 @@ in
     };
   };
 
+  # Service for fetching and color-filtering new wallpaper images
+  systemd.user.services."wallpaper-fetcher" =
+    let
+      palette-1 = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/catppuccin/catppuccin/03f1d0557c41331a61871a01b52e343315fdbff8/assets/palette/macchiato.png";
+        hash = "sha256-G4NkibJk5HGOcKh6jc1cJnQyqdNetX72ysxeFPirW/w=";
+      };
+      palette-2 = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/catppuccin/catppuccin/03f1d0557c41331a61871a01b52e343315fdbff8/assets/palette/night.png";
+        hash = "sha256-ZbKJoStbyd5lYukpuflN66UPNlEceRtBG1D+T8t669A=";
+      };
+      script = pkgs.writeShellScriptBin "fetch-wallpaper" ''
+        export PATH=$PATH:${lib.makeBinPath [ pkgs.coreutils pkgs.jq pkgs.curl pkgs.imagemagick ]}
+        export WD=/home/sevi/.cache/wallpaper-fetcher
+        export WPD=/home/sevi/Pictures/Wallpapers
+        export META=$(curl 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=2&mkt=en-US')
+        mkdir -p $WPD
+        mkdir -p $WD
+        curl -o $WD/wallpaper_raw_1.jpg "https://bing.com$(echo $META| jq -r '.images[0].url')"
+        curl -o $WD/wallpaper_raw_2.jpg "https://bing.com$(echo $META| jq -r '.images[1].url')"
+        convert -append ${palette-1} ${palette-2} $WD/palette.png
+        convert $WD/wallpaper_raw_1.jpg -map $WD/palette.png $WPD/wallpaper.png
+        convert $WD/wallpaper_raw_2.jpg -map $WD/palette.png $WPD/wallpaper2.png
+        echo "Wallpaper updated."
+      '';
+    in
+    {
+      Service = {
+        Type = "simple";
+        ExecStart = "${script}/bin/fetch-wallpaper";
+      };
+      Install.WantedBy = [ "default.target" ];
+    };
+
   # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
   # This folder needs to be manually deleted on rebuild for some reason
   # xdg.configFile = {
