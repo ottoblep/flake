934b8b78555dc396df018f5e1f3f16d294af5655
Severin Lochschmidt <seviron53@gmail.com>
2024-04-06 14:04:00 +0200

    Improve pygad package

--- pkgs/pygad/default.nix
+++ pkgs/pygad/default.nix
@@ -1,15 +1,43 @@
-{ pkgs, ... }:
-let
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pythonOlder
+, numpy
+, matplotlib
+, cloudpickle
+, torch
+, tensorflow
+, keras
+}:
+
+buildPythonPackage rec {
   pname = "pygad";
   version = "3.3.1";
-in
-pkgs.python3Packages.buildPythonPackage {
-  inherit pname version;
-  src = pkgs.fetchFromGitHub rec {
+  disabled = (pythonOlder "3.7");
+
+  src = fetchFromGitHub {
     owner = "ahmedfgad";
     repo = "GeneticAlgorithmPython";
-    rev = "${version}";
+    rev = version;
     sha256 = "sha256-Tie1gTcoHItGlqKr+wemh1m6KTbW8cEB1iDgEl7Wap8=ha256-Tie1gTcoHItGlqKr+wemh1m6KTbW8cEB1iDgEl7Wap8=";
   };
-  doCheck = false;
+
+  catchConflicts = false;
+
+  buildInputs = [
+    numpy
+    matplotlib
+    cloudpickle
+    torch
+    tensorflow
+    keras
+  ];
+
+  meta = with lib; {
+    description = "Library for building the genetic algorithm and optimizing machine learning algorithms";
+    homepage = "https://github.com/ahmedfgad/GeneticAlgorithmPython";
+    changelog = "https://github.com/ahmedfgad/GeneticAlgorithmPython/releases/tag/${version}";
+    license = licenses.bsd3;
+    maintainers = [ ];
+  };
 }
