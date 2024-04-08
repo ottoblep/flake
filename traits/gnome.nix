f3d98ccf1b9320f81a61471450f89886fc2fc178
Severin Lochschmidt <seviron53@gmail.com>
2024-04-08 16:44:15 +0200

    Add pam keyring unlock

--- traits/gnome.nix
+++ traits/gnome.nix
@@ -4,43 +4,44 @@
 { config, pkgs, lib, ... }:
 
 {
-  config = {
-    services = {
-      xserver = {
-        enable = true;
-        layout = "de";
-        displayManager.gdm.enable = true;
-        displayManager.autoLogin.enable = false;
-        desktopManager.gnome.enable = true;
-      };
-      gnome.gnome-keyring.enable = true;
-      dbus.packages = with pkgs; [ dconf ];
+  services = {
+    xserver = {
+      enable = true;
+      layout = "de";
+      displayManager.gdm.enable = true;
+      displayManager.autoLogin.enable = false;
+      desktopManager.gnome.enable = true;
     };
-    environment.gnome.excludePackages = (with pkgs; [
-      gnome-photos
-      gnome-tour
-    ]) ++ (with pkgs.gnome; [
-      cheese # webcam tool
-      gnome-music
-      gedit # text editor
-      epiphany # web browser
-      geary # email reader
-      gnome-characters
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-      yelp # Help view
-      gnome-contacts
-      gnome-initial-setup
-    ]);
-
-    environment.systemPackages = with pkgs.gnome; [
-      gnome-tweaks
-      gnome-characters
-    ];
-
-    programs.dconf.enable = true;
+    gnome.gnome-keyring.enable = true;
+    dbus.packages = with pkgs; [ dconf ];
   };
+
+  security.pam.services.gdm.enableGnomeKeyring = true;
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    gnome-characters
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+    yelp # Help view
+    gnome-contacts
+    gnome-initial-setup
+  ]);
+
+  environment.systemPackages = with pkgs.gnome; [
+    gnome-tweaks
+    gnome-characters
+  ];
+
+  programs.dconf.enable = true;
 }
 
