df504e6fd15ddede4449a1df9e9e373f69a9ec59
Severin Lochschmidt <seviron53@gmail.com>
2024-04-16 13:46:01 +0200

    Add virtualization to sevtp2

--- flake.nix
+++ flake.nix
@@ -140,6 +140,7 @@
                   traits.graphical
                   traits.gnome
                   traits.media
+                  traits.virtualization
                   users.sevi-full
                 ];
               };
