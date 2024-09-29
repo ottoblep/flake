c3735d8d26a6d0231a10b66a77c7e81375dc490d
Severin Lochschmidt <seviron53@gmail.com>
2024-09-29 17:01:39 +0200

    Add epianos

--- users/sevi/graphical.nix
+++ users/sevi/graphical.nix
@@ -15,25 +15,35 @@
     enable = true;
     package = pkgs.unstable.alacritty;
   };
-  # Can't yet use the builtin parser because it produces yaml
-  home.file.".config/alacritty/alacritty.toml".source = ./dotfiles/alacritty.toml;
-  home.file.".config/alacritty/catppuccin-macchiato.toml".source =
-    let
-      src = pkgs.fetchFromGitHub {
-        repo = "alacritty";
-        owner = "catppuccin";
-        rev = "071d73effddac392d5b9b8cd5b4b527a6cf289f9";
-        hash = "sha256-HiIYxTlif5Lbl9BAvPsnXp8WAexL8YuohMDd/eCJVQ8=";
-      };
-    in
-    "${src}/catppuccin-macchiato.toml";
 
-  home.file.".config/openrazer/persistence.conf".source = ./dotfiles/openrazer.conf;
-  home.file.".config/xournalpp/settings.xml".source = ./dotfiles/xournalpp.xml;
+  home.file = {
+    # Can't yet use the builtin parser because it produces yaml
+    ".config/alacritty/alacritty.toml".source = ./dotfiles/alacritty.toml;
+    ".config/alacritty/catppuccin-macchiato.toml".source =
+      let
+        src = pkgs.fetchFromGitHub {
+          repo = "alacritty";
+          owner = "catppuccin";
+          rev = "071d73effddac392d5b9b8cd5b4b527a6cf289f9";
+          hash = "sha256-HiIYxTlif5Lbl9BAvPsnXp8WAexL8YuohMDd/eCJVQ8=";
+        };
+      in
+      "${src}/catppuccin-macchiato.toml";
 
-  # LV2 Plugins for Carla
-  home.file.".lv2/b_synth".source = "${pkgs.setbfree}/lib/lv2/b_synth";
-  home.file.".lv2/b_whirl".source = "${pkgs.setbfree}/lib/lv2/b_whirl";
-  home.file.".lv2/fomp.lv2".source = "${pkgs.fomp}/lib/lv2/fomp.lv2";
-  home.file.".lv2/aether.lv2".source = "${pkgs.aether-lv2}/lib/lv2/aether.lv2";
+    ".config/openrazer/persistence.conf".source = ./dotfiles/openrazer.conf;
+    ".config/xournalpp/settings.xml".source = ./dotfiles/xournalpp.xml;
+
+    # LV2 Plugins for Carla
+    ".lv2/b_synth".source = "${pkgs.setbfree}/lib/lv2/b_synth";
+    ".lv2/b_whirl".source = "${pkgs.setbfree}/lib/lv2/b_whirl";
+    ".lv2/fomp.lv2".source = "${pkgs.fomp}/lib/lv2/fomp.lv2";
+    ".lv2/aether.lv2".source = "${pkgs.aether-lv2}/lib/lv2/aether.lv2";
+    ".lv2/sfizz.lv2".source = "${pkgs.sfizz}/lib/lv2/sfizz.lv2";
+    ".sfz/epianos".source = pkgs.fetchFromGitHub {
+      owner = "sfzinstruments";
+      repo = "GregSullivan.E-Pianos";
+      rev = "8c3e581acda3594b553948ff0222d4f84a698376";
+      sha256 = "sha256-tpPNz1ev6AcoTfPMGbfTTTu2wbp6OG1m0f6pCJi9KAM=";
+    };
+  };
 }
