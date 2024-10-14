a8e50f3bab5bdcdf3be980b91f5c7c74b154f821
Severin Lochschmidt <seviron53@gmail.com>
2024-10-14 21:14:45 +0200

    Add tips on zoom screen share

--- traits/comms.nix
+++ traits/comms.nix
@@ -5,7 +5,8 @@
 {
   config = {
     environment.systemPackages = with pkgs; [
-      pkgs.unstable.zoom-us
+      # Start with and enable screenshare of applications started with XDG_SESSION_TYPE=x11 GDK_BACKEND=x11
+      pkgs.zoom-us
       whatsapp-for-linux
       signal-desktop
     ];
