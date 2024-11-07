5ab26b2798beefecde6b35cce699a9f9c4fd0c43
Severin Lochschmidt <seviron53@gmail.com>
2024-11-07 12:53:58 +0100

    Fix alacritty config

--- users/sevi/graphical.nix
+++ users/sevi/graphical.nix
@@ -17,19 +17,8 @@
   };
 
   home.file = {
-    # Can't yet use the builtin parser because it produces yaml
     ".config/alacritty/alacritty.toml".source = ./dotfiles/alacritty.toml;
-    ".config/alacritty/catppuccin-macchiato.toml".source =
-      let
-        src = pkgs.fetchFromGitHub {
-          repo = "alacritty";
-          owner = "catppuccin";
-          rev = "071d73effddac392d5b9b8cd5b4b527a6cf289f9";
-          hash = "sha256-HiIYxTlif5Lbl9BAvPsnXp8WAexL8YuohMDd/eCJVQ8=";
-        };
-      in
-      "${src}/catppuccin-macchiato.toml";
-
+    ".config/alacritty/alacritty-macchiato.toml".source = ./dotfiles/alacritty-macchiato.toml;
     ".config/openrazer/persistence.conf".source = ./dotfiles/openrazer.conf;
     ".config/xournalpp/settings.xml".source = ./dotfiles/xournalpp.xml;
   };
