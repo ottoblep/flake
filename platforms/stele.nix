2a1feb965e45472b29a88916b5e86f3882fc0c16
Severin Lochschmidt <seviron53@gmail.com>
2025-07-01 21:31:49 +0200

    Fix screen flicker in wayland games

--- platforms/stele.nix
+++ platforms/stele.nix
@@ -76,7 +76,7 @@
         nvidiaSettings = true;
 
         # Optionally, you may need to select the appropriate driver version for your specific GPU.
-        package = config.boot.kernelPackages.nvidiaPackages.production;
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
       };
 
       hardware.nvidia-container-toolkit.enable = true;
