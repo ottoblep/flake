904de5e21a7fc5c93f083f6d653ae479743d593f
Severin Lochschmidt <seviron53@gmail.com>
2025-10-20 22:02:46 +0200

    Simplify gpu driver

--- platforms/stele.nix
+++ platforms/stele.nix
@@ -54,29 +54,8 @@
       services.xserver.videoDrivers = [ "nvidia" ];
 
       hardware.nvidia = {
-
-        # Modesetting is required.
-        modesetting.enable = true;
-
-        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-        powerManagement.enable = false;
-        # Fine-grained power management. Turns off GPU when not in use.
-        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-        powerManagement.finegrained = false;
-
-        # Use the NVidia open source kernel module (not to be confused with the
-        # independent third-party "nouveau" open source driver).
-        # Support is limited to the Turing and later architectures. Full list of 
-        # supported GPUs is at: 
-        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+        videoAcceleration = true;
         open = true;
-
-        # Enable the Nvidia settings menu,
-        # accessible via `nvidia-settings`.
-        nvidiaSettings = true;
-
-        # Optionally, you may need to select the appropriate driver version for your specific GPU.
-        package = config.boot.kernelPackages.nvidiaPackages.beta;
       };
 
       hardware.nvidia-container-toolkit.enable = true;
