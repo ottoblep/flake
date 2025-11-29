03cf5e67319773cc29c5c1cb848c2116aafa74bd
Severin Lochschmidt <seviron53@gmail.com>
2025-11-29 14:11:13 +0100

    Simplify module

--- pkgs/tflight4/default.nix
+++ pkgs/tflight4/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, lib, makeWrapper, kernel ? null, kernelModuleMakeFlags ? null }:
+{ stdenv, fetchFromGitHub, lib, makeWrapper, kernel, kernelModuleMakeFlags ? null }:
 
 let
   pname = "tflight4";
@@ -13,26 +13,26 @@ stdenv.mkDerivation rec {
     sha256 = "17qylkkzyx89ahqhf7qijk8x9j30hsj4fjn186qi1srchc03rmr4";
   };
 
-  nativeBuildInputs = [ makeWrapper ] ++ lib.optional (kernel != null) kernel.moduleBuildDependencies;
+  nativeBuildInputs = [ makeWrapper ] ++ kernel.moduleBuildDependencies;
 
   # Use the kernel's make flags and point the build to the kernel dev tree.
-  makeFlags = lib.optional (kernel != null) (kernel.makeFlags) ++ (lib.optional (kernel != null) [
+  makeFlags = kernel.makeFlags ++ [
     "KERNELDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
     "INSTALL_MOD_PATH=$(out)"
-  ]);
+  ];
 
   prePatch = ''
     substituteInPlace ./Makefile --replace '/lib/modules/$(shell uname -r)/build' "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
   '';
 
   installPhase = ''
-    mkdir -p $out/lib/modules/${if kernel == null then "$(uname -r)" else kernel.modDirVersion}/kernel/drivers/hid
+  mkdir -p $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/hid
     # Prefer the built object in the working directory, or the installed path created
     # by 'make INSTALL_MOD_PATH=$(out)' as $out/lib/modules/..../kernel/drivers/hid/hid-tflight4.ko
     if [ -f hid-tflight4.ko ]; then
-      cp hid-tflight4.ko $out/lib/modules/${if kernel == null then "$(uname -r)" else kernel.modDirVersion}/kernel/drivers/hid/
+  cp hid-tflight4.ko $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/hid/
     elif [ -f out/hid-tflight4.ko ]; then
-      cp out/hid-tflight4.ko $out/lib/modules/${if kernel == null then "$(uname -r)" else kernel.modDirVersion}/kernel/drivers/hid/
+  cp out/hid-tflight4.ko $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/hid/
     fi
   '';
 
