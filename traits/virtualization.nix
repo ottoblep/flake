bdda22d0d2a8e6da72bfab41a7b7be2b1f00d4bd
Severin Lochschmidt <seviron53@gmail.com>
2026-01-07 18:50:17 +0100

    Switch to podman

--- traits/virtualization.nix
+++ traits/virtualization.nix
@@ -3,10 +3,10 @@
 */
 { pkgs, ... }:
 {
-  virtualisation.docker = {
+  virtualisation.podman = {
       enable = true;
-      enableOnBoot = true;
-      package = pkgs.docker_25;
+      # enableOnBoot = true;
+      # package = pkgs.docker_25;
       # Nvidia Docker (deprecated)
       #enableNvidia = true;
   };
