007452bcd1aa6d19540e96d20c7033cea0ebed76
Severin Lochschmidt <seviron53@gmail.com>
2026-02-01 14:38:24 +0100

    Add required insecure

--- traits/nixos.nix
+++ traits/nixos.nix
@@ -37,6 +37,10 @@
     # Use devShell to define libraries
     programs.nix-ld.enable = true;
 
+    nixpkgs.config.permittedInsecurePackages = [
+      "qtwebengine-5.15.19"
+    ];    
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
