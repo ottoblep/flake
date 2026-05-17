4869cdbc63067ac44c782bb2e9635f62753ba91d
Severin Lochschmidt <seviron53@gmail.com>
2026-05-17 20:29:53 +0200

    Switch to smooth cursor alacritty

--- users/sevi/graphical.nix
+++ users/sevi/graphical.nix
@@ -13,7 +13,22 @@
 
   programs.alacritty = {
     enable = true;
-    package = pkgs.unstable.alacritty;
+    package = (pkgs.unstable.alacritty.overrideAttrs (old: rec {
+        version = "0.17.0-dev";
+        src = pkgs.fetchFromGitHub {
+          owner = "GregTheMadMonk";
+          repo = "alacritty-smooth-cursor";
+          rev = "master";
+          hash = "sha256-WgfXJYdCJDkcM2CJrIYWYUldpz6U/vgQIlEJKkNiFc0=";
+        };
+        cargoDeps = pkgs.unstable.rustPlatform.fetchCargoVendor {
+          inherit src;
+          hash = "sha256-pbDuSvlTEUdf23LFXxK17UsXUzTUQsnnypoduUdsm+c=";
+        };
+        postPatch = (old.postPatch or "") + ''
+          sed -i '614s/`/\"/g' extra/man/alacritty.5.scd
+        '';
+      }));
   };
 
   home.file = {
