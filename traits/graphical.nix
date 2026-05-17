2806213f909e5e3236a197c5b888ffdc1674eea8
Severin Lochschmidt <seviron53@gmail.com>
2026-05-17 21:11:32 +0200

    Start mouseless later

--- traits/graphical.nix
+++ traits/graphical.nix
@@ -17,7 +17,8 @@
 
     systemd.services.mouseless = {
       description = "Mouseless mouse replacement daemon";
-      wantedBy = [ "multi-user.target" ];
+      wantedBy = [ "graphical.target" ];
+      after = [ "display-manager.service" ];
       serviceConfig = {
         ExecStart = "${pkgs.mouseless}/bin/mouseless --config /home/sevi/.config/mouseless/config.yaml";
         Restart = "always";
